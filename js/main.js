98a01422bfbaa43054d61494f687af9915be527c
Tatiana Cohen <tatiana.cohen.arias@gmail.com>
2015-04-14 15:31:48 -0400

    Changed JS button function to jQuery

--- js/main.js
+++ js/main.js
@@ -20,20 +20,11 @@ $( document ).ready(function() {
 	});
 
 
+    $('.button').hover(
+          function(){this.src = this.src.replace("button","button-hover");},
+          function(){this.src = this.src.replace("button-hover","button");
+     });
 
-    var button = document.getElementById("button");
-	button.addEventListener("mouseover" , swap , false);
-	button.addEventListener("mouseout" , swapBack , false);
-
-	function swap (){
-		console.log("swaped");
-		button.src = "img/button-hover.png";
-	}
-
-	function swapBack (){
-		console.log("back");
-		button.src = "img/button.png";
-	}
 
 	// Call Gridder
 	    $('.gridder').gridderExpander({
