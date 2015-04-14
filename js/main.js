7f7d721acdcb4df701ff0f8ea2bd4833ffebea41
Tatiana Cohen <tatiana.cohen.arias@gmail.com>
2015-04-14 11:52:37 -0400

    Updated navigation for desktop and added scrollup plugin

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ $( document ).ready(function() {
     
     $('#menu').slicknav({
 		prependTo:'nav'
-});
+	});
+
+    $(function () {
+	  $.scrollUp({
+	    scrollName: 'scrollUp', 
+	    topDistance: '400', 
+	    topSpeed: 300, 
+	    animation: 'fade', 
+	    animationInSpeed: 200, 
+	    animationOutSpeed: 200, 
+	    scrollText: 'Scroll to top', 
+	    activeOverlay: false, 
+	  });
+	});
 
 });
