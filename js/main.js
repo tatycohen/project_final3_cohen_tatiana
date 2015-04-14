574b3b6ae199ee2c794ae8a31e9e98859780de44
Tatiana Cohen <tatiana.cohen.arias@gmail.com>
2015-04-14 12:18:41 -0400

    Added JS for button swap

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
     
+
     $('#menu').slicknav({
 		prependTo:'nav'
 	});
+
+	 smoothScroll.init();
 
     $(function () {
 	  $.scrollUp({
@@ -16,5 +19,20 @@ $( document ).ready(function() {
 	    activeOverlay: false, 
 	  });
 	});
+
+    var button = document.getElementById("button");
+	button.addEventListener("mouseover" , swap , false);
+	button.addEventListener("mouseout" , swapBack , false);
+
+	function swap (){
+		console.log("swaped");
+		button.src = "img/button-hover.png";
+	}
+
+	function swapBack (){
+		console.log("back");
+		button.src = "img/button.png";
+	}
+	
 
 });
